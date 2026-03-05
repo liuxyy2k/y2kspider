f3ab2d97c0d8ba3924e97c5a20d6e69299302e66
liuxyy2k <56387179+liuxyy2k@users.noreply.github.com>
2026-03-05 19:06:42 +0800

    Add files via upload

--- index.config.js
+++ index.config.js
@@ -100,10 +100,6 @@ var index_config_default = {
       {
         name: "🔞湿园",
         address: "https://xxavs.com/api.php/provide/vod/at/json"
-      },
-      {
-        name: "🔞聚合",
-        address: "http://192.168.0.101:3006/spider/m3uLive/3"
       }
     ]
   },
